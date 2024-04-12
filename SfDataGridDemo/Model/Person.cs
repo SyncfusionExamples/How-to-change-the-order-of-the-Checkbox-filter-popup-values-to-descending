40f0d04a3001058e160d47ceb7e9d433e864525a
Sweatha <sweatha.bharathi@syncfusion.com>
2024-04-12 18:35:00 +0530

    ES-881501 - To change the order of the Checkbox filter popup values to descending.

---
+++ SfDataGridDemo/Model/Person.cs
@@ -0,0 +1,66 @@
+﻿using Syncfusion.UI.Xaml.Core;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WINUI_DataGridDemos.Model
+{
+    public class OrderInfo
+    {
+        int orderID;
+        string customerId;
+        string country;
+        string customerName;
+        string shippingCity;
+        DateTime date;
+
+        public int OrderID
+        {
+            get { return orderID; }
+            set { orderID = value; }
+        }
+
+        public string CustomerID
+        {
+            get { return customerId; }
+            set { customerId = value; }
+        }
+
+        public string CustomerName
+        {
+            get { return customerName; }
+            set { customerName = value; }
+        }
+
+        public string Country
+        {
+            get { return country; }
+            set { country = value; }
+        }
+
+        public string ShipCity
+        {
+            get { return shippingCity; }
+            set { shippingCity = value; }
+        }
+
+        public DateTime Date
+        {
+            get { return date; }
+            set { date = value; }
+        }
+
+        public OrderInfo(int orderId, string customerName, string country, string customerId, string shipCity , DateTime date)
+        {
+            this.OrderID = orderId;
+            this.CustomerName = customerName;
+            this.Country = country;
+            this.CustomerID = customerId;
+            this.ShipCity = shipCity;
+            this.Date = date;
+        }
+    }
+}
